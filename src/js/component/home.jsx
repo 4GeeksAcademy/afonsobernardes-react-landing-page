e20c0204e3119cb155e09434870619243e55ad17
Afonso Bernardes <84087794+AfonsoBernardes@users.noreply.github.com>
2023-08-29 19:06:40 +0000

    Navbar toggler working with correct color.

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -16,19 +16,19 @@ const Home = () => {
 				<Jumbotron />
 
 				<div className="row d-flex justify-content-between mt-4">
-					<div className="col-sm-12 col-md-3 mt-2">
+					<div className="col-sm-12 col-md-3 mt-4">
 						<Card text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut tristique et egestas quis ipsum suspendisse ultrices gravida dictum. Aliquet lectus proin nibh nisl condimentum id venenatis a."} imageURL={"https://picsum.photos/500/400"}/>
 					</div>
 
-					<div className="col-sm-12 col-md-3 mt-2">
+					<div className="col-sm-12 col-md-3 mt-4">
 						<Card text={"In est ante in nibh mauris. Faucibus et molestie ac feugiat sed lectus vestibulum mattis ullamcorper. Malesuada fames ac turpis egestas. Eget felis eget nunc lobortis mattis aliquam faucibus. Dui id ornare arcu odio ut sem nulla pharetra."} imageURL={"https://picsum.photos/500/400"}/>
 					</div>
 
-					<div className="col-sm-12 col-md-3 mt-2">
+					<div className="col-sm-12 col-md-3 mt-4">
 						<Card text={"Turpis nunc eget lorem dolor sed viverra. Nullam ac tortor vitae purus faucibus ornare suspendisse. Quam pellentesque nec nam aliquam. Augue ut lectus arcu bibendum at varius. Dignissim sodales ut eu sem integer vitae justo eget magna."} imageURL={"https://picsum.photos/500/400"}/>
 					</div>
 
-					<div className="col-sm-12 col-md-3 mt-2">
+					<div className="col-sm-12 col-md-3 mt-4">
 						<Card text={"Consequat id porta nibh venenatis cras sed felis eget. Egestas erat imperdiet sed euismod. Nunc sed id semper risus. Arcu cursus euismod quis viverra nibh cras pulvinar mattis nunc."} imageURL={"https://picsum.photos/500/400"}/>
 					</div>
 				</div>
--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const Navbar = () => {
 	return (
-        <nav className="navbar navbar-expand-lg bg-dark py-3">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark py-3">
             <div className="container d-flex justify-content-between w-60">
                 
                 <a className="navbar-brand fs-3 text-white" href="#">Start Bootstrap</a>
                 
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-light navbar-toggler-icon"></span>
+                    <span className="navbar-toggler-icon"></span>
                 </button>
 
                 <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
