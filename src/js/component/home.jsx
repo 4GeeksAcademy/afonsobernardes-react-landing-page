6d959b41ef4c59f4c29160734174db5aeec11980
Afonso Bernardes <84087794+AfonsoBernardes@users.noreply.github.com>
2023-08-29 19:00:42 +0000

    Navbar responsive with navbar collapse working.

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -12,12 +12,10 @@ const Home = () => {
 		<div className="container-fluid p-0 mb-2">
 			<Navbar />
 
-			<div className="container d-flex w-60 pt-5">
+			<div className="container w-60 mt-3 pt-5">
 				<Jumbotron />
-			</div>
 
-			<div className="container d-flex w-60 pt-5">
-				<div className="row d-flex justify-content-between">
+				<div className="row d-flex justify-content-between mt-4">
 					<div className="col-sm-12 col-md-3 mt-2">
 						<Card text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut tristique et egestas quis ipsum suspendisse ultrices gravida dictum. Aliquet lectus proin nibh nisl condimentum id venenatis a."} imageURL={"https://picsum.photos/500/400"}/>
 					</div>
@@ -35,7 +33,6 @@ const Home = () => {
 					</div>
 				</div>
 			</div>
-			
 		</div>
 	);
 };
--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -8,10 +8,10 @@ const Navbar = () => {
                 <a className="navbar-brand fs-3 text-white" href="#">Start Bootstrap</a>
                 
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+                    <span className="navbar-light navbar-toggler-icon"></span>
                 </button>
 
-                <div className="collapse navbar-collapse d-flex justify-content-end" id="navbarNavAltMarkup">
+                <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
                     <div className="navbar-nav fs-5">
                         <a className="nav-link active text-white" aria-current="page" href="#">Home</a>
                         <a className="nav-link text-white" href="#">About</a>
